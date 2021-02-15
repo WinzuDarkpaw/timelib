5123b4e46f4901413ead11f93ba94f486d697191
WinzuDarkpaw <niconowak3@gmail.com>
2021-02-15 10:11:57 +0100

    neustart

--- main.c
+++ main.c
@@ -12,33 +12,6 @@ Nico Nowak
 
 int main()
 {
-    // Testcode
-    // Erzeugen der Date-Struktur, definiert in timelib.h
-    date userDate;
-    userDate.day = 0;
-    userDate.month = 0;
-    userDate.year = 0;
 
-    int daysOfTheYear = 0;
-
-    printf("Jahr: ");
-    scanf("%i", &userDate.year);
-    fflush(stdin);
-
-    printf("Monat: ");
-    scanf("%i", &userDate.month);
-    fflush(stdin);
-
-    printf("Tag: ");
-    scanf("%i", &userDate.day);
-    fflush(stdin);
-
-    userDate = normalize_date(userDate);
-    daysOfTheYear = day_of_the_year(userDate);
-
-    printf("Datum: %i.%i.%i\n", userDate.day, userDate.month, userDate.year);
-    printf("Das ist der %i. Tag des Jahres %i.", daysOfTheYear, userDate.year);
-    #ifdef DEBUG
-    #endif // DEBUG
     return 0;
 }
--- timelib.h
+++ timelib.h
@@ -1,20 +1,4 @@
 #ifndef TIMELIB_H_INCLUDED
 #define TIMELIB_H_INCLUDED
-// Typ-Definition der Struktur als date
-typedef struct
-{
-    int year;
-    int month;
-    int day;
-} date;
 
-// Variable: Int-Array mit 12 Zahlen
-extern int daysPerMonth[12];
-
-// Funktionsprototypen
-int is_leapyear(int year);
-date normalize_date(date dateInput);
-void normalize_leaped_february(int year);
-int day_of_the_year(date dateInput);
-void input_date(int* day, int* month, int* year);
 #endif // TIMELIB_H_INCLUDED
