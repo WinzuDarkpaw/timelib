08e3b6b1fa8d8686eb48b35f6584856f98e914c5
WinzuDarkpaw <niconowak3@gmail.com>
2021-02-15 11:03:39 +0100

    Funktionsprototypen und Konstanten

--- timelib.h
+++ timelib.h
@@ -3,7 +3,7 @@
 /**
 
 Timelib.h
-Header-Datei für timelib.c
+Header-Datei fï¿½r timelib.c
 Beinhaltet Funktionsprototypen und symbolische Konstanten zum arbeiten mit
 Daten.
 
@@ -11,56 +11,61 @@ Autor: Nico Nowak
 Erstellungsdatum: 15.02.2021
 **/
 
+/**     SYMBOLISCHE KONSTANTEN  **/
+#define TRUE 1
+#define FALSE 0
+#define ERROR -1
+
 /**     FUNKTIONSPROTOTYPEN     **/
 
 /*
     Prototyp
-    Die Funktion berechnet für ein gegebens Datum des gregorianischen Kalendars
-    bestehend aus Tag, Monat und Jahr die Nummer des Tages, gezählt von Jahresbeginn
+    Die Funktion berechnet fï¿½r ein gegebens Datum des gregorianischen Kalendars
+    bestehend aus Tag, Monat und Jahr die Nummer des Tages, gezï¿½hlt von Jahresbeginn
     (1. Januar) an.
-    Schaltjahre werden bei der Berechnung berücksichtigt. Ist das übergebene Datum
-    ungültig, beträgt der Rückgabewert -1.
+    Schaltjahre werden bei der Berechnung berï¿½cksichtigt. Ist das ï¿½bergebene Datum
+    ungï¿½ltig, betrï¿½gt der Rï¿½ckgabewert -1.
 */
 int day_of_the_year(int day, int month, int year);
 
 /*
     Prototyp
-    Die Funktion liest 3 Ganzahlwerte (Integer) ein, für Tag, Monat und Jahr.
-    Wenn das angegebene Daten ungültig ist, wird erneut eingelesen, solange
-    bis ein gültiges Datum eingegeben wurde.
+    Die Funktion liest 3 Ganzahlwerte (Integer) ein, fï¿½r Tag, Monat und Jahr.
+    Wenn das angegebene Daten ungï¿½ltig ist, wird erneut eingelesen, solange
+    bis ein gï¿½ltiges Datum eingegeben wurde.
 */
 void input_date(int* day, int* month, int* year);
 
 /*
     Prototyp
-    Die Funktion überprüft, ob ein gegebenes Jahr nach den Regeln des
+    Die Funktion ï¿½berprï¿½ft, ob ein gegebenes Jahr nach den Regeln des
     gregorianischen Kalenders ein Schaltjahr ist. Bei Jahreszahlen
-    vor dem Jahr 1582 wird ein Fehler zurückgegeben.
-    Rückgabewerte:
-    1, wenn übergebenes Jahr ein Schaltjahr ist
-    0, wenn übergebenes Jahr kein Schaltjahr ist
-    -1, wenn ein ungültiges Jahr übergeben wurde
+    vor dem Jahr 1582 wird ein Fehler zurï¿½ckgegeben.
+    Rï¿½ckgabewerte:
+    1, wenn ï¿½bergebenes Jahr ein Schaltjahr ist
+    0, wenn ï¿½bergebenes Jahr kein Schaltjahr ist
+    -1, wenn ein ungï¿½ltiges Jahr ï¿½bergeben wurde
 */
 int is_leapyear(int year);
 
 /*
     Prototyp
-    Die Funktion bestimmt für einen gegebenen Monat eines gegebenen Jahres, wie viele
+    Die Funktion bestimmt fï¿½r einen gegebenen Monat eines gegebenen Jahres, wie viele
     Tage der Monat hat. Der Wert des Monats muss zwischen 1 und 12 liegen.
-    Schaltjahre werden berücksichtigt.
-    Rückgabewerte:
-    1-31, wenn ein gültiger Monat eingegeben wurde
-    -1, wenn ein ungültiger Monat oder ein ungültiges Jahr eingegeben wurde
+    Schaltjahre werden berï¿½cksichtigt.
+    Rï¿½ckgabewerte:
+    1-31, wenn ein gï¿½ltiger Monat eingegeben wurde
+    -1, wenn ein ungï¿½ltiger Monat oder ein ungï¿½ltiges Jahr eingegeben wurde
 */
 int get_days_for_month(int month, int year);
 
 /*
     Prototyp
-    Die Funktion überprüft, ob ein eingegebenes Datum gültig ist.
-    Daten vor dem 1.1.1582 sind ungültig, genauso wie alle Daten nach dem
+    Die Funktion ï¿½berprï¿½ft, ob ein eingegebenes Datum gï¿½ltig ist.
+    Daten vor dem 1.1.1582 sind ungï¿½ltig, genauso wie alle Daten nach dem
     31.12.2400.
-    1, wenn gültig
-    0, wenn ungültig
+    1, wenn gï¿½ltig
+    0, wenn ungï¿½ltig
 */
 int exists_date(int day, int month, int year);
 
